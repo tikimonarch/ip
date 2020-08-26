be9aae1fa5ad9276d9b27c737868f07e9829df23
tikimonarch <tikimonarch@users.noreply.github.com>
2020-08-27 03:35:53 +0800

    Level-1
    Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,8 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 public class Duke {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -8,6 +11,15 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         String border = "____________________________________________________________\n";
         System.out.print(border + " Hello! I'm Duke\n" + " What can I do for you?\n" + border);
-        System.out.print(" Bye. Hope to see you again soon!\n" + border);
+        String command;
+        while (true) {
+            BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+            command = read.readLine();
+            if (command.equals("bye")) {
+                break;
+            }
+            System.out.print(border + " " + command + "\n" + border);
+        }
+        System.out.print(border + " Bye. Hope to see you again soon!\n" + border);
     }
 }
