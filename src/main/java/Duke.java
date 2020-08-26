b50c55dbf81e2c95110212a64cc114e48ff744df
tikimonarch <tikimonarch@users.noreply.github.com>
2020-08-27 04:08:58 +0800

    Level-2
    Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,13 +12,29 @@ public class Duke {
         String border = "____________________________________________________________\n";
         System.out.print(border + " Hello! I'm Duke\n" + " What can I do for you?\n" + border);
         String command;
+        int size = 0;
+        String[] list = new String[100];
         while (true) {
             BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
             command = read.readLine();
             if (command.equals("bye")) {
                 break;
+            } else if (command.equals("list")){
+                if (size == 0){
+                    System.out.print(border + "Empty\n" + border);
+                }
+                else {
+                    System.out.print(border);
+                    for (int i = 0; i < size; i++) {
+                        System.out.println(" " + Integer.toString(i + 1) + ". " + list[i]);
+                    }
+                    System.out.print(border);
+                }
+                continue;
             }
-            System.out.print(border + " " + command + "\n" + border);
+            list[size] = command;
+            size++;
+            System.out.print(border + " added: " + command + "\n" + border);
         }
         System.out.print(border + " Bye. Hope to see you again soon!\n" + border);
     }
